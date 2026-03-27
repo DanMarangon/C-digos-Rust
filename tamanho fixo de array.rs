113814c2c95010e302d567e192dd698247d48851
Dan <danramosmarques123@gmail.com>
2026-03-27 14:23:46 -0300

    Add fixed-size array example in Rust

---
+++ tamanho fixo de array.rs
@@ -0,0 +1,13 @@
+fn main() {
+let meses: [&str;4] = ["Janeiro","Fevereiro","Março","Abril"];
+
+println!("{:?}",meses);
+println!("{:?}",meses[0]);
+println!("{:?}",meses[3]);
+
+
+let zeros = [0;5];
+
+println!("{:?}",zeros);
+
+}
